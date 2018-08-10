ffcb7e0f6f0c2cbde51bd615e4c878ae80f24b84
maelgrove <maelgrove@gmail.com>
2018-08-11 00:24:49 +0200

    Added test to verify whether values are written only back to their store of origin

--- src/Config.Net.Tests/MultiStoreTest.cs
+++ src/Config.Net.Tests/MultiStoreTest.cs
@@ -2,51 +2,65 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Config.Net.Json.Stores;
 using Config.Net.Stores;
 using Config.Net.Stores.Impl.CommandLine;
+using Config.Net.Yaml.Stores;
 using Xunit;
 
 namespace Config.Net.Tests
 {
-    public class MultiStoreTest : AbstractTestFixture, IDisposable
+    public class MultiStoreTest : AbstractTestFixture
     {
-       private string _testFilePath;
-       private IniFileConfigStore _iniFileConfigStore;
-       private CommandLineConfigStore _commandLineConfigStore;
-
-       public MultiStoreTest()
-       {
-          //get back clean file
-          string src = Path.Combine(BuildDir.FullName, "TestData", "sample.ini");
-          _testFilePath = Path.Combine(TestDir.FullName, "sample.ini");
-          File.Copy(src, _testFilePath, true);
-
-          //create the stores
-          _iniFileConfigStore = new IniFileConfigStore(_testFilePath, true, true);
-          _commandLineConfigStore = new CommandLineConfigStore(new []{ "-key0:value1" });
-       }
-
        [Fact]
        public void Read_config_values_in_correct_order()
        {
-          IMultiStoreSettings settings = new ConfigurationBuilder<IMultiStoreSettings>()
-             .UseConfigStore(_iniFileConfigStore)
-             .UseConfigStore(_commandLineConfigStore)
-             .Build();
+         using (IConfigStore iniFileConfigStore = new IniFileConfigStore(GetSamplePath("ini"), true, true))
+         using (IConfigStore commandLineConfigStore = new CommandLineConfigStore(new[] { "-key0:value1" }))
+         {
+            IMultiStoreSettings settings = new ConfigurationBuilder<IMultiStoreSettings>()
+               .UseConfigStore(iniFileConfigStore)
+               .UseConfigStore(commandLineConfigStore)
+               .Build();
 
-          Assert.Equal("value1", settings.Key0);
+            Assert.Equal("value1", settings.Key0);
+         }
        }
 
-       /// <inheritdoc />
-       public void Dispose()
+       [Fact]
+       public void Write_config_values_back_to_origin()
        {
-          _iniFileConfigStore.Dispose();
-          _commandLineConfigStore.Dispose();
+         using (IConfigStore iniFileConfigStore = new IniFileConfigStore(GetSamplePath("ini"), true, true))
+         using (IConfigStore yamlFileConfigStore = new YamlFileConfigStore(GetSamplePath("yml")))
+         {
+            IMultiStoreSettings settings = new ConfigurationBuilder<IMultiStoreSettings>()
+               .UseConfigStore(iniFileConfigStore)
+               .UseConfigStore(yamlFileConfigStore)
+               .Build();
+
+            settings.Key0 = "value2";
+
+            Assert.Null(yamlFileConfigStore.Read("key0"));
+         }
+      }
+
+       protected string GetSamplePath(string ext)
+       {
+          string dir = BuildDir.FullName;
+          string src = Path.Combine(dir, "TestData", "sample." + ext);
+          string testFile = Path.Combine(dir, src);
+          src = Path.GetFullPath(testFile);
+          string dest = Path.Combine(TestDir.FullName, "sample." + ext);
+
+          File.Copy(src, dest, true);
+
+          return dest;
        }
-    }
+
+   }
 
    public interface IMultiStoreSettings
    {
-      string Key0 { get; }
+      string Key0 { get; set; }
    }
 }
